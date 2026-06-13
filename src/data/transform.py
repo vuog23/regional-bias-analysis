f2c8e65cbbaba9a2a0b7a11c0c1ca6320220a343
Vuog23 <vuog23082004@gmail.com>
2026-06-13 19:50:08 +0700

    add data transformation

---
+++ src/data/transform.py
@@ -0,0 +1,25 @@
+from torchvision import transforms
+
+def train_transform():
+    return transforms.Compose([
+        transforms.RandomResizedCrop(224, scale=(0.7, 1.0)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
+        transforms.RandomGrayscale(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        ),
+    ])
+
+
+def eval_transform():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        ),
+    ])
